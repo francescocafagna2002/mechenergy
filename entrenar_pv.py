8870e1948b02ec3f7772f90576a7bd495161317c
Beltran Albert <beltranalbert@outlook.es>
2026-09-11 02:32:28 +0000

    entrenar_pv

---
+++ entrenar_pv.py
@@ -0,0 +1,21 @@
+import pandas as pd
+from load import cargar_tablas, construir_vector_labels
+from model import entrenar_modelo, evaluar_modelo, explicar_prediccion
+
+# 1. Cargar la matriz real que te dieron (ojo: la columna se llama "mp_id" en minúsculas)
+X = pd.read_csv("labelled_features.csv", index_col="mp_id")
+
+# 2. Cargar labels
+gigi, zgp, mapping = cargar_tablas(
+    "../aew-data/test-blob/input_data/HackDays2026 - GIGI.csv",
+    "../aew-data/test-blob/input_data/Zähler-GP.csv",
+    "../aew-data/test-blob/input_data/mpid_zähler_mapping.csv",
+)
+y_pv = construir_vector_labels(gigi, zgp, mapping, columna_activo=" PV")
+
+# 3. Entrenar y evaluar
+modelo_pv, X_test, y_test = entrenar_modelo(X, y_pv, nombre="PV")
+evaluar_modelo(modelo_pv, X_test, y_test, nombre="PV")
+
+# 4. Explicar una casa concreta
+explicar_prediccion(modelo_pv, X, mp_id=X.index[0])
